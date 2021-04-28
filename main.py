3b1a81612eb1d93341e4f81a50f1c92df80db1ff
Chandler Swift <chandler@chandlerswift.com>
2021-04-27 19:21:34 -0500

    Add basic crash handling

--- main.py
+++ main.py
@@ -20,26 +20,31 @@ lastfm_top_tracks = network.get_user("chandlerswift").get_top_tracks(stream=True
 track_data=[]
 
 for i, lastfm_track in enumerate(lastfm_top_tracks):
-    spotify_track = spotify.search(
-        f"{lastfm_track.item.artist.name} {lastfm_track.item.title}", types=["track"], limit=1
-    ).tracks[0]
+    try:
+        spotify_track = spotify.search(
+            f"{lastfm_track.item.artist.name} {lastfm_track.item.title}", types=["track"], limit=1
+        ).tracks[0]
 
-    res = requests.get(f"http://localhost:8080/albumPlayCount?albumid={spotify_track.album.id}").json()
+        res = requests.get(f"http://localhost:8080/albumPlayCount?albumid={spotify_track.album.id}").json()
 
-    found_track = None
-    for spotify_disc in res['data']['discs']:
-        for spotify_track_info in spotify_disc['tracks']:
-            if spotify_track_info['name'].lower() == lastfm_track.item.title.lower():
-                found_track = spotify_track_info
+        found_track = None
+        for spotify_disc in res['data']['discs']:
+            for spotify_track_info in spotify_disc['tracks']:
+                if spotify_track_info['name'].lower() == lastfm_track.item.title.lower():
+                    found_track = spotify_track_info
 
-    if found_track:
-        found_track['my_playcount'] = lastfm_track.weight
-        track_data.append(found_track)
-        print(f"{found_track['name']}: {found_track['my_playcount']}/{found_track['playcount']} ({100*found_track['my_playcount']/found_track['playcount']:.3f}%)")
-    else:
-        print(f"No track {lastfm_track.item.title} found on album {spotify_track.album.name} (found {[[t['name'] for t in d['tracks']] for d in res['data']['discs']]})")
+        if found_track:
+            found_track['my_playcount'] = lastfm_track.weight
+            track_data.append(found_track)
+            print(f"{found_track['name']}: {found_track['my_playcount']}/{found_track['playcount']} ({100*found_track['my_playcount']/found_track['playcount']:.3f}%)")
+        else:
+            print(f"No track {lastfm_track.item.title} found on album {spotify_track.album.name} (found {[[t['name'] for t in d['tracks']] for d in res['data']['discs']]})")
 
-    time.sleep(2) # I don't think Spotify's going to ban me, but doesn't hurt
-                  # to be a little extra careful!
+        time.sleep(2) # I don't think Spotify's going to ban me, but doesn't hurt
+                    # to be a little extra careful!
+    except Exception as e:
+        # Sometimes things crash. This might give me a chance to continue, if I want.
+        print(e)
+        input("continue? ")
 
 pickle.dump(track_data, open("track_data.pickle", "wb"))
